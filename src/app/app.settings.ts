0860355cfc1d0b28ca7aa42273485313e62f5449
Ahmet Enis Güven <118840111+ahmetenisguven@users.noreply.github.com>
2024-10-04 20:43:11 +0300

    Update app.settings.ts

--- src/app/app.settings.ts
+++ src/app/app.settings.ts
@@ -4,8 +4,8 @@ export class AppSettings {
     // Comment below line for local development
     // return 'http://164.90.180.104/api';
     // Uncomment below line for local development
-    return 'http://127.0.0.1:5000';
-    //return 'https://metabolitics.itu.edu.tr/api';
+    //return 'http://127.0.0.1:5000';
+    return 'https://metabolitics.itu.edu.tr/api';
   }
 
   public static get NOTIFICATION_OPTIONS() {
