47b16b8174a02878dbb7925b72b976b5a34a4b57
ahmetenisguven <ahmetenisguven@gmail.com>
2025-03-04 02:08:30 +0300

    Fragment fix.

--- src/demo/demo.component.ts
+++ src/demo/demo.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, AfterViewInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
-  selector: 'app-documentation',
+  selector: 'app-demo',
   templateUrl: './demo.component.html',
   styleUrls: ['./demo.component.css']
 })
-export class DemoComponent implements OnInit {
+export class DemoComponent implements AfterViewInit {
+  constructor(private route: ActivatedRoute) {}
 
-  constructor() { }
-
-  ngOnInit() {
+  ngAfterViewInit() {
+    this.route.fragment.subscribe(fragment => {
+      if (fragment) {
+        const element = document.getElementById(fragment);
+        if (element) {
+          element.scrollIntoView({ behavior: 'smooth', block: 'start' });
+        }
+      }
+    });
   }
-
 }
